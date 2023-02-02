20735b16af54c406edb2c7d8fa80e57b1fca6d79
frenzy <vsabirov@proton.me>
2023-02-02 18:27:35 +0300

    feat: initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.scss';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      hi
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+
+import App from './components/App';
+
+const root: ReactDOM.Root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <App />
+);
